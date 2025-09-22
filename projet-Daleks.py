e1e6a06f09722e680008fc6688ac7f899a22d4ed
felix <larosefelixb@gmail.com>
2025-09-22 14:28:05 -0400

    debut d'une fonction pour bouger une entité

--- projet-Daleks.py
+++ projet-Daleks.py
@@ -10,15 +10,42 @@ class Docteur:
         self.positionX = x
         self.positionY = y
 
-    class dalek:
-        def __init__(self, vie, deplacement, valeur, x, y):
-            self.vieDalek = vie
-            self.deplacementDalek = deplacement
-            self.valeurCosmique = valeur
-            self.position = [x, y]
+class dalek:
+    def __init__(self, vie, deplacement, valeur, x, y):
+        self.vieDalek = vie
+        self.deplacementDalek = deplacement
+        self.valeurCosmique = valeur
+        self.position = [x, y]
 
+docSymbole = "D"
+vide = "#"
+dalekSymbole = "X"
+feraille = "o"
 
+def afficher_grille(grille):
+    for ligne in grille:
+        print("".join(ligne))
 
+grille = [
+    [docSymbole, vide, vide, vide, vide, vide, vide, vide, vide, vide],
+    [vide, vide, vide, vide, vide, vide, vide, dalekSymbole, vide, vide],
+    [vide, dalekSymbole, vide, vide, vide, vide, vide, vide, vide, vide],
+    [vide, vide, vide, vide, vide, vide, vide, vide, dalekSymbole, vide],
+]
 
+afficher_grille(grille)
 
+direction = ["up", "right", "down", "left"]
 
+def mooveEntity(x, y, deplacement):
+    random = randint(0, 3)
+    if direction[random] == "up":
+        y -= 1
+    elif direction[random] == "right":
+        x += 1 
+    elif direction[random] == "down":
+        y += 1
+    elif direction[random] == "left":
+        x -= 1
+    if grille[y[x]] == vide :
+        print("cum")

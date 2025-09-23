ac6721c46fe87c3c2beaf82eacd91b5d62529fae
felix <larosefelixb@gmail.com>
2025-09-23 17:33:18 -0400

    burg

--- projet-Daleks.py
+++ projet-Daleks.py
@@ -1,5 +1,7 @@
 from random  import randint     # retourne un entier aléatoire entre 2 numéros inclus
 
+
+#  Définition des deux classes principal et nécéssaire
 class Docteur:
     def __init__(self, vie, deplacement, x, y):
         self.vieDocteur = vie
@@ -15,6 +17,8 @@ class Dalek:
         self.vieDalek = vie
         self.deplacementDalek = deplacement
         self.valeurCosmique = valeur
+        self.positionX = x
+        self.positionY = y
         self.position = [x, y]
 
 docSymbole = "D"
@@ -35,28 +39,61 @@ grille = [
 
 afficher_grille(grille)
 
-collectionDalek = []
-dalek1 = Dalek(2,3,4,5,6)
-dalek1.position = 2
-dalek2 = Dalek(2,3,4,5,6)
-dalek3 = Dalek(2,3,4,5,6)
-collectionDalek.append(dalek1)
-collectionDalek.append(dalek2)
-collectionDalek.append(dalek3)
-collectionDalek[1]
+def creerDalek(vie, deplacement, valeur, x, y):
+    dalek = Dalek(vie,1,5,5,5)
+    collectionDalek.append(dalek)
+    grille[y][x] = dalekSymbole
 
 
+# liste de tout les dalek
+collectionDalek:list[Dalek] = []
+# comment creer un nouveau dalek
+dalek = Dalek(2,3,4,5,6)
+# rajouter le dalek a la liste
+collectionDalek.append(dalek)
+
+print(collectionDalek[0].valeurCosmique)
+
+def detruire_Dalek(x1,y1, ):
+    global Dalek
+    detruire = False
+    compteurBoucle = 0
+    for Dalek in collectionDalek:
+        if collectionDalek[compteurBoucle].position[0] == x and collectionDalek[compteurBoucle].position[1] == y:
+            detruire = True
+            id = compteurBoucle
+        compteurBoucle += 1
+    if detruire:
+       del collectionDalek[id]
+
+
+def deplacer_docteur(grille, nouveauX, nouveauY):
+    global docteurAncienX
+    global docteurAncienY
+    grille[docteurAncienY][docteurAncienX] = vide
+    grille[nouveauY][nouveauX] = docSymbole
+    docteurAncienX = nouveauX
+    docteurAncienY = nouveauY
+
+#  PREND UN NOMBRE RANDOM ENTRE 0 ET 3 (0 = UP, 1 = RIGHT, 2 = DOWN, 3 = LEFT)
+def deplacer_dalek(dalek: Dalek, direction):
+    if direction == 0 and grille[dalek.position[0]][dalek.position[0]] == vide:
+        x -= 1        
+    elif direction[random] == "right":
+        x += deplacement 
+    elif direction[random] == "down":
+        y += deplacement
+    elif direction[random] == "left":
+        x -= deplacement
 
 def mooveEntity(x, y, deplacement):
     direction = ["up", "right", "down", "left"]
     random = randint(0, 3)
-    if direction[random] == "up":
-        y -= 1
-    elif direction[random] == "right":
-        x += 1 
-    elif direction[random] == "down":
-        y += 1
-    elif direction[random] == "left":
-        x -= 1
+    
     if grille[y[x]] == vide :
         print("cum")
+
+
+creerDalek(1,1,1,0,0)
+detruire_Dalek(0,0)
+
